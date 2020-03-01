0ed37207d6f2c1fb4d53cdfc2f0dacc30968a846
Arttu Viljakainen <arttu.viljakainen@gmail.com>
2020-03-01 15:56:43 +0200

    Made article titles links.

--- src/components/gallery-article.js
+++ src/components/gallery-article.js
@@ -3,7 +3,10 @@ import { Link } from "gatsby"
 
 const GalleryArticle = ({ post }) => (
   <article className="article">
-    <h3>{post.frontmatter.title}</h3>
+      <Link to={post.frontmatter.path}>
+        <h3>{post.frontmatter.title}</h3>
+      </Link>
+    
     {post.frontmatter.summary !== null && (<p>{post.frontmatter.summary}</p>)} 
     {post.frontmatter.summary === null && (<p>{post.excerpt}</p>)} 
     <div className="article-read-more">
