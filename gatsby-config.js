5e1dc42a32c8f5a798e2283ab141dae11e735787
Arttu Viljakainen <arttu.viljakainen@gmail.com>
2020-04-26 23:14:09 +0300

    Added google analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
     author: `@arttuv`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-101290596-1",
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
