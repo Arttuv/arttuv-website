5b29aa164180d8767db5324578d117a4757f59cc
Arttu Viljakainen <arttu.viljakainen@gmail.com>
2020-05-03 11:59:30 +0300

    Multiple fixes to mailchimp subscription.

--- src/components/mailchimp-subscribe.js
+++ src/components/mailchimp-subscribe.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import addToMailchimp from 'gatsby-plugin-mailchimp';
 import ReCAPTCHA from "react-google-recaptcha";
+import { Link } from "gatsby"
 
 const MailchimpState = {
     ADVERTISE: "advertise",
@@ -12,23 +13,31 @@ const MailchimpState = {
 const MailchimpSubscribe = ({ siteTitle, small}) => {
 
     const [mailchimpState, setMailchimpState] = useState(MailchimpState.ADVERTISE);
-    const [verified, setVerified ] = useState(false);
     const [email, setEmail] = useState("");
+    const [consent, setConsent] = useState("N");
     const [result, setResult] = useState(null);
     
     const handleSubmit = async( event ) => {
+        event.preventDefault();
+
         if( email.length > 0) {
-            const result = await addToMailchimp(email, {});
+            var fields = {};
+            fields['gdpr[25193]'] = consent;
+            
+            const result = await addToMailchimp(email, fields);
             setResult(result);
             setMailchimpState(MailchimpState.FINISHED);
     
             console.log(result);    
         }
-        event.preventDefault();
     }
 
     const handleChange = event => {
         setEmail( event.target.value );
+    }
+
+    const handleChangeConsent = event => {
+        setConsent( event.target.value );
     }
 
     const recaptchaCallback = value => {
@@ -39,16 +48,20 @@ const MailchimpSubscribe = ({ siteTitle, small}) => {
             setMailchimpState(MailchimpState.VERIFIED);
         }
      }
-    
+
+     function createMarkup(result) {
+        return {__html: result.msg};
+      }
+          
     return (
         <>
         <div className="rssSubscribe">
             <h1>RSS</h1>
             <div>
                 <p>
-                <a href="arttuv.com/rss.xml">RSS Link</a>
+                    <a href="/rss.xml">RSS Link</a>
                 </p>
-                <p>Yes, RSS still exists. Full text is provided in RSS so you'll never need to visit this site if you don't want to.</p>
+                <p>Yes, RSS still exists \o/ Full text is provided in RSS so you'll never need to visit this site if you don't want to.</p>
             </div>
         </div>
 
@@ -59,7 +72,7 @@ const MailchimpSubscribe = ({ siteTitle, small}) => {
                 We use Mailchimp as our marketing platform. By clicking below to subscribe, you acknowledge that your information will be transferred to Mailchimp for processing. <a href="https://mailchimp.com/legal/" target="_blank">Learn more about Mailchimp's privacy practices here.</a>
             </p>
             {mailchimpState === MailchimpState.ADVERTISE && (
-                <p><a href="#footer" onClick={() => setMailchimpState(MailchimpState.SHOW_CAPTCHA)}>Verify</a> ReCaptcha to show the subscription form.</p>
+                <p><a href="#footer" onClick={() => setMailchimpState(MailchimpState.SHOW_CAPTCHA)}>Verify ReCaptcha</a> to show the subscription form.</p>
             )}
             {mailchimpState === MailchimpState.SHOW_CAPTCHA && (
                 <ReCAPTCHA className="reCaptchaContainer" sitekey="6LcjgvEUAAAAAJ-rYjKkLeWoLi1ThewAIyvNf5Ij" onChange={recaptchaCallback} size="normal"/>
@@ -69,16 +82,16 @@ const MailchimpSubscribe = ({ siteTitle, small}) => {
                 <input type="email" placeholder="type your email" value={email} onChange={handleChange} />
                 <h2>Marketing Permissions</h2>
                 <p>Please confirm you would like to hear from Arttu Viljakainen by:</p>
-                <fieldset class="mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
-                <label class="checkbox subfield" for="gdpr_25193">Email
-                    <input type="checkbox" id="gdpr_25193" name="gdpr[25193]" value="Y" class="av-checkbox gdpr" required/>
+                <fieldset className="mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
+                <label className="checkbox subfield" htmlFor="gdpr_25193">Email
+                    <input type="checkbox" id="gdpr_25193" name="gdpr[25193]" value="Y" onChange={handleChangeConsent} className="av-checkbox gdpr" required/>
                 </label>
                 </fieldset>
                 <input className="mailchimpSubscribeButton" type="submit" value="Subscribe" />
             </form>
             )}
-            {result !== null && (
-                <p>{result.msg}</p>
+            {mailchimpState === MailchimpState.FINISHED && (
+                <p className="mailchimpResponse" dangerouslySetInnerHTML={createMarkup(result)}></p>
             )}
         </div>
         </>
