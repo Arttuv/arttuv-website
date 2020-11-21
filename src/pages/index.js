081e49e774b92fa7f6ebbd21e6ac804c03000dd8
Arttu Viljakainen <Arttu@medias-MacBook-Pro.local>
2020-11-22 00:00:05 +0200

    Removing different theme from "graphical" pages.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import WritingsGallery from "../components/writings-gallery"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import BorderedTitle from "../components/bordered-title"
 import ImageFace from "../components/image-face"
